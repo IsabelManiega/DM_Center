794a04eda7648ce45fea53fe1301e4119d780b84
cjcuenca <cjcuenca@gmail.com>
2022-10-19 11:23:25 +0200

    connection con ISOLATION

--- connection.py
+++ connection.py
@@ -1,12 +1,17 @@
 import settings
 import psycopg2
 
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
 def connect():
     conn = psycopg2.connect(database=settings.DATABASE,
                             user=settings.USER,
                             password=settings.PASSWORD,
                             host=settings.HOST,
                             port=settings.PORT)
+
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
     cur = conn.cursor()
     return cur, conn
