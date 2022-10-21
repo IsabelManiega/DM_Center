81a1f71990dd65676a788309960a7eb157a5ac4c
JGN10 <jero.gutierrez.n@gmail.com>
2022-10-21 10:37:49 +0200

    cambios en el main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from models import User
 from connection import connect
 
 # Funciones
-def mostrar(tabla, column, valor, response):
+def mostrar_modificado(tabla, column, valor, response):
         # Conexión a base de datos PostgreSQL
         cur, conn = connect()
         rows = []
@@ -112,7 +112,7 @@ async def deleteOne(id: int, response: Response):
     response.status_code = status.HTTP_404_NOT_FOUND
     return {"id": id, "msg":"User Not Found"}
 
-@app.delete("/deleteData/", tags=["PENDIENTES"],
+@app.delete("/deleteData/", tags=["PENDIENTESssssss"],
             description="Eliminar todos usuario")
 async def delete(response: Response):
     database.clear()
