6d30c3a45cb480f128d30bf97e1e846c464847fa
cjcuenca <cjcuenca@gmail.com>
2022-10-26 12:00:58 +0200

    Cambios cambio GET id paso a diccionario

--- mostrardatostabla.py
+++ mostrardatostabla.py
@@ -1,16 +1,23 @@
 import psycopg2
 from connection import connect
+from fastapi import FastAPI, status, Response
 
 def mostrar(tabla, column, valor, response):
         # Conexión a base de datos PostgreSQL
         cur, conn = connect()
         rows = []
+        datos={}
 
         # Generamos y ejecutamos la query
         try:
             query = f"SELECT * FROM {tabla} WHERE {column} = {valor};"
             cur.execute(query)
             rows = cur.fetchall()
+            datos["Id"]=rows[0][0]
+            datos["Nombre"]=rows[0][1]
+            datos["Edad"]=rows[0][2]
+            datos["Nota"]=rows[0][3]
+            datos["Fecha"]=rows[0][4]
         except psycopg2.Error as e:
             error = "Error mostrar registros: %s" + str(e)
             return {f"msg":error}
@@ -25,4 +32,4 @@ def mostrar(tabla, column, valor, response):
             return {"id":valor , f"msg":"User Not Found"}
         else:
             response.status_code = status.HTTP_200_OK
-            return rows
+            return datos
