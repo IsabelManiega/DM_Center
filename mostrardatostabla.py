4e0372cbda213f5392ced9e5273f180937c7967d
cjcuenca <cjcuenca@gmail.com>
2022-10-26 12:10:55 +0200

    Cambio a diccionario salida GET Id

--- mostrardatostabla.py
+++ mostrardatostabla.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, status, Response
 def mostrar(tabla, column, valor, response):
         # Conexión a base de datos PostgreSQL
         cur, conn = connect()
-        rows = []
         datos={}
 
         # Generamos y ejecutamos la query
@@ -13,23 +12,23 @@ def mostrar(tabla, column, valor, response):
             query = f"SELECT * FROM {tabla} WHERE {column} = {valor};"
             cur.execute(query)
             rows = cur.fetchall()
-            datos["Id"]=rows[0][0]
-            datos["Nombre"]=rows[0][1]
-            datos["Edad"]=rows[0][2]
-            datos["Nota"]=rows[0][3]
-            datos["Fecha"]=rows[0][4]
+            if rows == []:
+                response.status_code = status.HTTP_404_NOT_FOUND
+                return {"id":valor , f"msg":"User Not Found"}
+            else:
+                datos["Id"]=rows[0][0]
+                datos["Nombre"]=rows[0][1]
+                datos["Edad"]=rows[0][2]
+                datos["Nota"]=rows[0][3]
+                datos["Fecha"]=rows[0][4]                
+                response.status_code = status.HTTP_200_OK
+                cur.close()
+                conn.close()
+                return datos
+
         except psycopg2.Error as e:
             error = "Error mostrar registros: %s" + str(e)
+            cur.close()
+            conn.close()  
             return {f"msg":error}
-        # Actualizamos y cerramos la base de datos
-        conn.commit()
-        cur.close()
-        conn.close()
-
-        # Solo necesario este control para controlar que devuelva valores vacios
-        if rows == []:
-            response.status_code = status.HTTP_404_NOT_FOUND
-            return {"id":valor , f"msg":"User Not Found"}
-        else:
-            response.status_code = status.HTTP_200_OK
-            return datos
+        
